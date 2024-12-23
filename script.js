ec8f7448395cba21b5785b930e2deb01ecba1837
Skymerald <arthur.terrillon.sauvageot@gmail.com>
2024-12-23 19:28:37 +0100

    first version of this vitrine site with the logo, name and slogan with animation

---
+++ script.js
@@ -0,0 +1,26 @@
+var i = 0;
+var txt = 'Innovation starts here'; /* The text */
+var speed = 170; /* The speed/duration of the effect in milliseconds */
+let display = true
+
+function typeWriter() {
+  if (i < txt.length) {
+    document.querySelector(".slogan").innerHTML = document.querySelector(".slogan").innerHTML.replace("▮", "");
+    document.querySelector(".slogan").innerHTML += txt.charAt(i) + "▮";
+    setTimeout(typeWriter, speed);
+    i++;
+  }
+  else{
+    if(display == true){
+        document.querySelector(".slogan").innerHTML = document.querySelector(".slogan").innerHTML.replace("▮", "");
+        setTimeout(typeWriter, 1000);
+        display = false;
+    }
+    else if(display == false){
+        document.querySelector(".slogan").innerHTML += "▮";
+        setTimeout(typeWriter, 1000);
+        display = true;
+    }
+  }
+}
+addEventListener(onload, typeWriter());

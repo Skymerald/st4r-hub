73de3f107d35bbd304e7eadf9399b26364b6df8d
Skymerald <arthur.terrillon.sauvageot@gmail.com>
2024-12-24 18:58:23 +0100

    upgrade of animation, and add of fix

--- script.js
+++ script.js
@@ -1,26 +1,87 @@
 var i = 0;
-var txt = 'Innovation starts here'; /* The text */
+let y = 0;
+let x = 0;
+var txt = ['Innovation starts here', 'web developpement']; /* The text */
+let z = txt[0].length;
 var speed = 170; /* The speed/duration of the effect in milliseconds */
 let display = true
+let loop = 0;
+let iterationTime = 10;
 
 function typeWriter() {
-  if (i < txt.length) {
-    document.querySelector(".slogan").innerHTML = document.querySelector(".slogan").innerHTML.replace("▮", "");
-    document.querySelector(".slogan").innerHTML += txt.charAt(i) + "▮";
-    setTimeout(typeWriter, speed);
-    i++;
+  switch(loop){
+    case 0 :
+      write(txt[0], iterationTime);
+    break;
+    case 1 :
+      deleteText(txt[0], 5);
+    break;
+    case 2:
+      write(txt[1], iterationTime);
+    break;
+    case 3:
+      deleteText(txt[1], 5);
+    break;
   }
-  else{
-    if(display == true){
+}
+function write(text, iteration){
+  if(y < text.length + iteration){
+    if (i < text.length) {
+      document.querySelector(".slogan").innerHTML = document.querySelector(".slogan").innerHTML.replace("▮", "");
+      document.querySelector(".slogan").innerHTML += text.charAt(i) + "▮";
+      setTimeout(typeWriter, speed);
+      i++;
+    }
+    else{
+      if(display == true){
+          document.querySelector(".slogan").innerHTML = document.querySelector(".slogan").innerHTML.replace("▮", "");
+          setTimeout(typeWriter, 600);
+          display = false;
+      }
+      else if(display == false){
+          document.querySelector(".slogan").innerHTML += "▮";
+          setTimeout(typeWriter, 600);
+          display = true;
+      }
+    }
+    y++;
+  }
+  if(y == text.length + iteration){
+    loop++;
+    x =0;
+  }
+}
+function deleteText(text, deleteIteration){
+  y = 0;
+  i = 0;
+  if(x < text.length + deleteIteration){
+    if (z != 0) {
+      document.querySelector(".slogan").innerHTML = document.querySelector(".slogan").innerHTML.replace(text[z-1]+"▮", "");
+      document.querySelector(".slogan").innerHTML += "▮";
+      setTimeout(typeWriter, speed);
+      z--;
+    }
+    else{
+      if(display == true){
         document.querySelector(".slogan").innerHTML = document.querySelector(".slogan").innerHTML.replace("▮", "");
         setTimeout(typeWriter, 600);
         display = false;
-    }
-    else if(display == false){
+      }
+      else if(display == false){
         document.querySelector(".slogan").innerHTML += "▮";
         setTimeout(typeWriter, 600);
         display = true;
+      }
     }
+    x++;
+  }
+  if(x == text.length + deleteIteration && loop !=3){
+    loop++;
+    z = txt[1].length;
+  }
+  else if(x == text.length + deleteIteration && loop ==3){
+    loop = 0;
+    z = txt[0].length;
   }
 }
 addEventListener(onload, typeWriter());
